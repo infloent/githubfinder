5c9e73ded7a35b9ef1eadb24b34b128c3f8ca648
infloent <infloent@yahoo.com>
2020-03-11 12:48:37 +0200

    First Commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor() {
+        this.client_id = 'f7cdd7748b8b660cebe7';
+        this.client_secret = '2322d222677481cda5d8a91fcce9320e109f1236';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
